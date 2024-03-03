e202dc2cab9b76b6051e094c13b38d0eb8cb7d93
haybuka <paschal.nduleo@gmail.com>
2024-03-03 16:00:08 +0100

    card

---
+++ src/app/components/card/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Card = () => {
+  return <div className="border-weesher-300 border p-1 rounded-md">Card</div>;
+};
+
+export default Card;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Inter, Playfair_Display } from 'next/font/google';
 import cls from 'classnames';
 import ArrowIcon from './icons/arrow';
 import Banner from './components/banner';
+import Card from './components/card';
 
 const playFair = Playfair_Display({ weight: '900', subsets: ['latin'] });
 const inter = Inter({ subsets: ['latin'] });
@@ -10,6 +11,11 @@ export default function Home() {
   return (
     <main>
       <Banner />
+      <section className="grid grid-cols-12 ">
+        {[1, 2, 3, 4].map((id) => (
+          <Card key={id} />
+        ))}
+      </section>
     </main>
   );
 }
